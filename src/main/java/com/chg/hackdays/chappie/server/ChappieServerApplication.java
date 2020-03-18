e2ba36ec8de786d265ee541afc1917b843037bc0
Brandon Lyon <etherous@gmail.com>
2020-03-18 10:43:39 -0600

    Initial commit

---
+++ src/main/java/com/chg/hackdays/chappie/server/ChappieServerApplication.java
@@ -0,0 +1,13 @@
+package com.chg.hackdays.chappie.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChappieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChappieServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chg/hackdays/chappie/server/ChappieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chg.hackdays.chappie.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChappieServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
